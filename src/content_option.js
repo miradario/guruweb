17563ca5b0a3ff3a9b1cc3fdf5fafa95268381ed
miradario <miradario@gmail.com>
2023-02-01 18:31:21 -0300

    commit

---
+++ src/content_option.js
@@ -0,0 +1,125 @@
+const logotext = "Gurudevelopers";
+const meta = {
+  title: "Gurudevelopers",
+  description: "INNOVATIVE SOLUTIONS",
+};
+
+const introdata = {
+  title: "We are Gurudevelopers",
+  animated: {
+    first: "We are a team of creative and innovative designers and developers",
+    second: "We develop websites and mobile apps",
+    third: "We love what we do",
+  },
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
+  your_img_url:
+    "https://gurudevelopers.dev/wp-content/uploads/2023/02/Logoweb.png",
+};
+
+const dataabout = {
+  title: "INNOVATIVE SOLUTIONS",
+  aboutme:
+    "We constantly do research to keep updating our tools and knowledge, incorporating the latest advances. We currently, have one foot in Web 2, slowly but surely, walking toward Web 3 projects and becoming experts in the blockchain world. We are an interdisciplinary team that interacts to offer the best solutions to our clients and advise them in assembling their IT solutions.",
+};
+const worktimeline = [
+  {
+    jobtitle: "Designer of week",
+    where: "YAdfi",
+    date: "2020",
+  },
+  {
+    jobtitle: "Designer of week",
+    where: "Jamalya",
+    date: "2019",
+  },
+  {
+    jobtitle: "Designer of week",
+    where: "ALquds",
+    date: "2019",
+  },
+];
+
+const skills = [
+  {
+    name: "Apps React Native",
+    value: 90,
+  },
+  {
+    name: "React Js and Next Js",
+    value: 85,
+  },
+  {
+    name: "Blockchain and Web3",
+    value: 80,
+  },
+  {
+    name: "Metaverse",
+    value: 75,
+  },
+];
+
+const services = [
+  {
+    title: "Mobile Apps",
+    description:
+      "Native app development 2D/3D Games Available for iOS and Android devices",
+  },
+  {
+    title: "Website ",
+    description: "100% Responsive sites. UI/UX Design. E-commerce features,",
+  },
+  {
+    title: "Metaverse",
+    description: "Decentraland Modeler, Decentraland SDK, Lands on sale",
+  },
+];
+
+const dataportfolio = [
+  {
+    img: "https://gurudevelopers.dev/wp-content/uploads/2023/02/iPhone-11-in-the-hand-of-a-man-beside-the-paintings_Screen-Shot-2022-09-16-at-14.25.22-1030x687.png",
+    desctiption: "Tlex Flow App. Meditation and Mindfulness App",
+    link: "https://gurudevelopers.com.ar/wp-content/uploads/2019/10/WhatsApp-Video-2022-09-20-at-1.05.22-PM.mp4",
+  },
+  {
+    img: "https://gurudevelopers.dev/wp-content/uploads/2023/02/Diseño-sin-título.png",
+    desctiption:
+      "Brainbo. Meditate with colors. Brainbo is a meditation app that uses colors to help you relax and meditate.",
+    link: "https://gurudevelopers.com.ar/wp-content/uploads/2022/09/Brainbo-Demo.mp4",
+  },
+  {
+    img: "https://gurudevelopers.dev/wp-content/uploads/2023/02/moeviius1-1030x687.png",
+    desctiption: "Moevius. React Native Game",
+    link: "#",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "info@gurudevelopers.com.ar",
+  YOUR_FONE: "+54 911 5644 5275",
+  description: "Book a 15 minutes discovery call",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_id",
+  YOUR_TEMPLATE_ID: "template_id",
+  YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+  github: "https://github.com/gurudevelopers",
+  facebook: "https://facebook.com/gurudevelopers",
+  linkedin: "https://www.linkedin.com/company/guru.developers/",
+  twitter: "https://twitter.com",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
