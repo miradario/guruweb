c42a8f01389829e99fbf55db90fac7d0a84ccccc
Agustin Sobral <asobralr@gmail.com>
2023-02-02 13:26:41 -0300

    Multiple fixes

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -14,11 +14,11 @@ export const Home = () => {
           <title> {meta.title}</title>
           <meta name="description" content={meta.description} />
         </Helmet>
-        <div className="intro_sec d-block d-lg-flex align-items-center ">
+        <div className="intro_sec d-block d-lg-flex align-items-center">
           <div
             className="h_bg-image order-1 order-lg-2 h-100 "
             style={{
-              backgroundImage: `url(${introdata.your_img_url})`,
+              // backgroundImage: `url(${introdata.your_img_url})`,
               backgroundSize: "contain",
             }}></div>
           <div className="text order-2 order-lg-1 h-100 d-lg-flex justify-content-center">
